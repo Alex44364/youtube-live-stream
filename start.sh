6d4a1d1de8b2863d67adb401cc210753ddfa191d
Alex44364 <mralexrech@gmail.com>
2026-08-24 23:04:27 +0400

    Update and rename start.sh to .github/workflows/streamA.yml

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while true; do
-    ffmpeg -re -stream_loop -1 -i 'https://github.com/Alex44364/youtube-live-stream/releases/download/v1/InShot_20260824_122903219.mp4' -c:v libx264 -preset veryfast -pix_fmt yuv420p -c:a aac -f flv 'rtmp://a.rtmp.youtube.com/live2/3g7j-2yx1-wp1h-0zgy-6dbd'
-    sleep 5
-done
